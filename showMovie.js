2f5470987a682da0af37a4cd57756a0d12d455fb
Celshia <celshiasahayam@gmail.com>
2020-11-22 20:03:11 +0530

    Update showMovie.js

--- showMovie.js
+++ showMovie.js
@@ -1,6 +1,7 @@
 class ShowMovie {
     movie = function (id, val) {
-        var data = movieObject['' + val + ''];
+        //var data = movieObject['' + val + ''];
+        let data = JSON.parse(localStorage.getItem("MOVIES"))[val].filter(obj=>obj.id ==id);
         var mainContainer = document.getElementById('movie-book');
 
         for (var i = 0; i < data.length; i++) {
@@ -32,6 +33,7 @@ class ShowMovie {
                 var seatcontainer = document.createElement("div");
                 seatcontainer.setAttribute('class','seat-container');
 
+                let seatNo = 1;
                 for (var z = 0; z < 4; z++) {
                     var seats = document.createElement("div");
                     seats.setAttribute('class', 'seats');
@@ -42,10 +44,14 @@ class ShowMovie {
                             //     p.innerHTML = ' ';
                             //     seats.appendChild(p);
                             // }
+                            //let seatNo = `${z}${x}${y}`;
                             var ip = document.createElement("input");
                             ip.setAttribute('type', 'checkbox');
+                            ip.setAttribute('name',"seats")
+                            ip.setAttribute('value',seatNo)
                             ip.setAttribute('id', z+''+x + '' + y);
                             seats.appendChild(ip);
+                            seatNo++;
                         }
                         var br = document.createElement("br");
                         seats.appendChild(br);
