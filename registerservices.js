0fbc377a44cc74a7ced4fe66f04d5b7e77f6c3c0
Celshia <celshiasahayam@gmail.com>
2020-11-30 21:10:52 +0530

    Update registerservices.js

--- registerservices.js
+++ registerservices.js
@@ -8,33 +8,11 @@ class RegisterService{
        location.reload();
        console.log(userObj);
        alert("continue login ");
-      /*  let result="success";
-       return result; */
+       let result="success";
+       return result;
     }
     else{
         alert("user already exists ");
     }
         }
 }
-
-/* 
-
-function register(){
-    event.preventDefault();
-    var userData={
-        email: document.getElementById("email").value,
-     name:document.getElementById("username").value,
-     password: document.getElementById("password").value
-    };
-    var usersr=JSON.parse(localStorage.getItem("USERS")) || [];
-    if(!usersr.some(user=>user.email===userData.email)){
-        usersr.push(userData);
-        localStorage.setItem("USERS",JSON.stringify(usersr));
-           location.reload();
-          console.log(userData);
-          alert("continue login ");
-    }
-    else{
-        alert("user already exists try registering with different credentials");
-    }
-} */
