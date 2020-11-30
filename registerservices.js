698d22e460c2e8f60f94fbca11f073bc65eb5625
Celshia <celshiasahayam@gmail.com>
2020-11-30 20:47:26 +0530

    Update registerservices.js

--- registerservices.js
+++ registerservices.js
@@ -2,9 +2,39 @@ class RegisterService{
     register = function(userObj){
        var usersTemp = JSON.parse(localStorage.getItem("USERS"));
        var users= usersTemp ? usersTemp:[];
+       if(!users.some(user=>user.email===userObj.email)){
        users.push(userObj);
        localStorage.setItem("USERS",JSON.stringify(users));
-       let result="success";
-       return result;
+       location.reload();
+       console.log(userObj);
+       alert("continue login ");
+      /*  let result="success";
+       return result; */
+    }
+    else{
+        alert("user already exists ");
+    }
         }
 }
+
+/* 
+
+function register(){
+    event.preventDefault();
+    var userData={
+        email: document.getElementById("email").value,
+     name:document.getElementById("username").value,
+     password: document.getElementById("password").value
+    };
+    var usersr=JSON.parse(localStorage.getItem("USERS")) || [];
+    if(!usersr.some(user=>user.email===userData.email)){
+        usersr.push(userData);
+        localStorage.setItem("USERS",JSON.stringify(usersr));
+           location.reload();
+          console.log(userData);
+          alert("continue login ");
+    }
+    else{
+        alert("user already exists try registering with different credentials");
+    }
+} */
